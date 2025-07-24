ca2370a89c966d497035f46e577c62cba0f69224
Ahmad Raza <ahmadsajidmughal357@gmail.com>
2025-07-24 13:34:17 +0500

    Fix origin in Socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const httpServer = http.createServer(app)
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://localhost:5173"]
+        origin: ["https://chat-app-frontend-neon-xi.vercel.app"]
     }
 });
 
